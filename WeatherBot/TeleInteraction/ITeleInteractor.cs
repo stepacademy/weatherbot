31e7baf535f10ce99e64cb9eb5bdea13797dcacc
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-04-14 07:01:48 +0300

    add: simple guide

--- WeatherBot/TeleInteraction/ITeleInteractor.cs
+++ WeatherBot/TeleInteraction/ITeleInteractor.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WeatherBot.TeleInteraction {
 
     public interface ITeleInteractor {
+
+        /// <summary>
+        /// GetUpdatesAsync(); - A temporary dummy, will be return the built-in type or Generic
+        /// </summary>
+
+        // Task<Telegram.Bot.Types.Update[]> GetUpdatesAsync();
+
+        // 1. use: Package Manager Console > "Restore" button
+        // 2. uncomment line 16 for test correctly restore packages
+
+        // otherwise:
+        // 1. use Package Manager Console
+        // 2. PM > Install-Package Telegram.Bot
 
     }
 }
