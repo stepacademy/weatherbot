f7f7db9466e221ecd29f1b684db18c532bd42d27
Евгений Владимирович Сотский <jekakmail@gmail.com>
2016-04-16 15:19:37 +0300

    Create WSLio

---
+++ WeatherBot/WSLio/IService1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WeatherBot.WSLio
+{
+    [DataContract]
+    public class QData
+    {
+        [DataMember] string City;
+
+        [DataMember] Dictionary<DateTime, QDataWeatherDay> itemsDays;
+    }
+
+    [DataContract]
+    public class QDataWeatherDay
+    {
+        [DataMember] List<QDataWeatherDayPart> itemsParts;
+    }
+
+    [DataContract]
+    public class QDataWeatherDayPart
+    {
+        [DataMember] double temp;
+        [DataMember] string dayPart;
+        [DataMember] string state;
+    }
+
+    public interface IModuleIO
+    {
+         
+    }
+
+
+
+    public interface IWeatherDbQuery
+    {
+        public QDataWeatherDay GetWeatherDay(DateTime dataTime);
+        public QDataWeatherDay GetWeatherCityDataWeatherDay(string city);
+
+
+    }
+}
