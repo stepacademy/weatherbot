4dcf9265e3dafb97a1f34f90585ccbd098e12d67
Vladas Zakrevskis <146100@gmail.com>
2016-04-16 14:38:30 +0300

    added database context

---
+++ WeatherBot/Database/WeatherDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherBot.Database.Entities
+{
+    class WeatherDbContext : DbContext
+    {
+        public WeatherDbContext() : base("ConnectionString")
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Weather> Weathers { get; set; }
+        public DbSet<WeatherDate> WeatherDates { get; set; }
+        public DbSet<WeatherState> WeatherStates { get; set; }
+
+    }
+}
