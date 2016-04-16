e305a599bbe976eca07785a0bc87199ee8a6e63b
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-04-16 03:01:50 +0300

    add: Initialize in to TeleInteractor

--- WeatherBot/TeleInteraction/TeleInteractor.cs
+++ WeatherBot/TeleInteraction/TeleInteractor.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Threading.Tasks;
 
 namespace WeatherBot.TeleInteraction {
@@ -12,12 +9,37 @@ namespace WeatherBot.TeleInteraction {
 
         private Telegram.Bot.Api _bot;
         private Telegram.Bot.Types.User _me;
+        private Telegram.Bot.Types.Update[] _updates;
+
+        public string BotName {
+            get {
+                return _me.FirstName;
+            }
+        }
 
         public async Task<Message> GetNextMessageAsync() {
-            
+
             //dummy await
+            System.Threading.Thread.Sleep(2400);
 
             return new Message();
+        }
+
+        private void Initialize(string tokenPath = "botToken.txt") {
+
+            using (StreamReader file = new StreamReader(tokenPath)) {
+                if ((_botToken = file.ReadLine()) != null) {
+
+                    _bot = new Telegram.Bot.Api(_botToken);
+                    _me = _bot.GetMe().Result;
+
+                }
+                //file.Close();
+            }
+        }
+
+        public TeleInteractor() {
+            Initialize();
         }
     }
 }
