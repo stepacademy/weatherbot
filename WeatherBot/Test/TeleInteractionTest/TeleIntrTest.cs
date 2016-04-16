eebcf2629ea2703648ca1549baae809250c256df
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-04-16 20:36:16 +0300

    impl: TeleIntrTest, fix: Paths

---
+++ WeatherBot/Test/TeleInteractionTest/TeleIntrTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WeatherBot.TeleInteraction;
+
+namespace Test.TeleInteractionTest {
+
+    class TeleIntrTest {
+
+        static async void TestProcess(ITeleInteractor ti) {
+
+            Message m = await ti.GetNextMessageAsync();
+            Console.WriteLine(m.Text);
+        }
+
+        static void Main(string[] args) {
+
+            ITeleInteractor test = new TeleInteractor();
+
+            while (true) {
+                TestProcess(test);
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+    }
+}
