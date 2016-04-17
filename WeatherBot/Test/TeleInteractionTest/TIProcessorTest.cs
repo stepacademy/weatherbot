f929ebf3eb7e59bc8b902e0c737aa75dd87cec0a
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-04-17 19:52:38 +0300

    add singleton: InteractionProcess

---
+++ WeatherBot/Test/TeleInteractionTest/TIProcessorTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.TeleInteractionTest {
+
+    class TIProcessorTest {
+
+        static void Main(string[] args) {
+
+            UsingTIProcessorExample test = new UsingTIProcessorExample();
+        }
+    }
+}
---
+++ WeatherBot/Test/TeleInteractionTest/UsingTIProcessorExample.cs
@@ -0,0 +1,28 @@
+﻿using WeatherBot.TeleInteraction;
+using WeatherBot.TeleInteraction.TelegramAdapters;
+
+namespace Test.TeleInteractionTest {
+
+    class UsingTIProcessorExample {
+
+        private Message MessageProcessing(Message message) {
+
+            if (message != null) {
+                message.Response = new MessageResponse();
+                message.Response.Text = "Using ReqResp Processor Example, Weather Bot Result from fake DB";
+
+                return message;
+            }
+            return null;
+        }
+
+        // HARDCODE c-tor =)
+        public UsingTIProcessorExample() {
+
+            InteractionProcess.Instance.ProcessingEventHandlers += MessageProcessing;
+            InteractionProcess.Instance.State = InteractionProcessState.Launched;
+            InteractionProcess.Instance.Process();
+        }
+
+    }
+}
