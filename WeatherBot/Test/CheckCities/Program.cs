bf40e532f94fbed540adabff7d295b195384bd4b
jekakmail <jekakmail@gmail.com>
2016-04-17 19:41:52 +0300

    change checkCities

--- WeatherBot/Test/CheckCities/Program.cs
+++ WeatherBot/Test/CheckCities/Program.cs
@@ -8,10 +8,59 @@ using System.Xml;
 
 namespace Test.CheckCities
 {
+    static class Progress
+    {
+        public static int Curr;
+        public static int TotalProgress;
+        private static object _thisLock = new object();
+
+        public static void DrawTextProgressBar(string msg)
+        {
+            lock (_thisLock)
+            {
+                Console.Clear();
+                
+                //draw empty progress bar
+                Console.CursorLeft = 0;
+                Console.Write("["); //start
+                Console.CursorLeft = 32;
+                Console.Write("]"); //end
+                Console.CursorLeft = 1;
+                float onechunk = 30.0f/TotalProgress;
+
+                //draw filled part
+                int position = 1;
+                for (int i = 0; i < onechunk*Curr; i++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.CursorLeft = position++;
+                    Console.Write(" ");
+                }
+
+                //draw unfilled part
+                for (int i = position; i <= 31; i++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.CursorLeft = position++;
+                    Console.Write(" ");
+                }
+
+                //draw totals
+                Console.CursorLeft = 35;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(Curr + " of " + TotalProgress + "\n"); //blanks at the end remove any excess
+
+                Console.WriteLine(msg);
+            }
+        }
+    }
+
     class Program
     {
         private static Semaphore _pool;
 
+        
+        private static StringBuilder _msg = new StringBuilder();
         static void Main(string[] args)
         {
 
@@ -19,15 +68,17 @@ namespace Test.CheckCities
 
         }
 
+        
+
         static void Worker(object countryElement)
         {
-            var country = (XmlElement)countryElement;
+            var country = (XmlElement) countryElement;
 
-            Console.WriteLine("{0} ждет", country.GetAttribute("name"));
+            // Console.WriteLine("{0} ждет", country.GetAttribute("name"));
 
             _pool.WaitOne();
 
-            Console.WriteLine("Старт {0}", country.GetAttribute("name"));
+            //Console.WriteLine("Старт {0}", country.GetAttribute("name"));
 
             foreach (XmlNode cityNode in country)
             {
@@ -42,13 +93,21 @@ namespace Test.CheckCities
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("----no load city: {0}, country: {1}----", cityNode.InnerText, country.GetAttribute("name"));
-                    Console.WriteLine("\t{0}", ex.Message);
+                    _msg.AppendFormat("no load id: {3, -5} city: {0,-25} country: {1,-15} {2,-20}", cityNode.InnerText,
+                        country.GetAttribute("name"), ex.Message, cityNode.Attributes.GetNamedItem("id").InnerText);
+                    _msg.AppendLine();
                 }
             }
 
             _pool.Release();
-            Console.WriteLine("Готово {0}", country.GetAttribute("name"));
+
+            Progress.Curr++;
+            Progress.DrawTextProgressBar(_msg.ToString());
+
+            
+
+
+            //Console.WriteLine("Готово {0}", country.GetAttribute("name"));
         }
 
         static void UpdateCities()
@@ -69,6 +128,8 @@ namespace Test.CheckCities
             
             //свободных 2
             _pool = new Semaphore(2, 10);
+
+            Progress.TotalProgress = doc.DocumentElement.ChildNodes.Count;
 
             foreach (XmlElement countryElement in doc.DocumentElement)
             {
