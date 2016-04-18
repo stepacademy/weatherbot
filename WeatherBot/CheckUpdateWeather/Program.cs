e087134b38fef37c441ccdf2b1dd69c0f4100919
jekakmail <jekakmail@gmail.com>
2016-04-18 19:03:22 +0300

    add checkUpdateWeather project

---
+++ WeatherBot/CheckUpdateWeather/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace WeatherBot.CheckUpdateWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var proxy = new WSLweatherReference.WeatherClient();
+
+            proxy.UpdateWeather();
+        }
+    }
+}
