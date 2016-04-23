bc3d06356da1ded9daae5d884fe29e55843d0b92
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-04-23 03:45:28 +0300

    implemented: WebHook-based interaction mode, NEED TESTS

--- WeatherBot/TeleInteraction/InteractionStrategy/BasedWebHook.cs
+++ WeatherBot/TeleInteraction/InteractionStrategy/BasedWebHook.cs
@@ -2,19 +2,59 @@
 /// Jeka, please Don't use ReSharper on this source file! Thanks. - Art.Stea1th.
 ///
 
-using System;
+using Owin;
+using System.Web.Http;
+using System.Threading;
 using System.ServiceModel;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Message = WeatherBot.TeleInteraction.Adapters.Message;
 
 namespace WeatherBot.TeleInteraction.InteractionStrategy {
 
-    internal class BasedWebHook : IInteractionStrategy {
+    internal class BasedWebHook : IInteractionStrategy {                   // WebHook-based interaction mode, NEED TESTS
 
-        public void Start() {
-            throw new NotImplementedException();
+        private IMessageProcessorCallback _currentOperationContext;
+
+        private Task Process() {
+
+            using (WebApp.Start<WebHookStartup>("https://+:8443")) {
+
+                try {                                                                                // Register WebHook
+                    Bot.Api.SetWebhook("https://azure.stepacademy.weatherbot:8443/WeatherBotWebHook").Wait();
+                    Thread.Sleep(Timeout.Infinite);
+                }
+                catch (ThreadInterruptedException) {                            // Thread Interrupt & Unregister WebHook
+                    Bot.Api.SetWebhook().Wait();
+                }
+            }
+            return new Task(null);
+        }
+
+        public async void Start() {
+            _currentOperationContext = OperationContext.Current.GetCallbackChannel<IMessageProcessorCallback>();
+
+            if (Thread.CurrentThread.ThreadState != ThreadState.WaitSleepJoin)
+                await Process();
         }
 
         public void Stop() {
-            throw new NotImplementedException();
+            if (Thread.CurrentThread.ThreadState == ThreadState.WaitSleepJoin)
+                Thread.CurrentThread.Interrupt();
+        }
+
+        public void Receive(Telegram.Bot.Types.Update update) {
+            _currentOperationContext.CallbackInvoke(new Message(update));
+        }
+    }
+
+    internal class WebHookStartup {
+
+        public void Configuration(IAppBuilder app) {
+
+            var configuration = new HttpConfiguration();
+            configuration.Routes.MapHttpRoute("WebHook", "{controller}");
+            app.UseWebApi(configuration);
         }
     }
 }
