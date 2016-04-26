735ac6d1a54642272ce608d6b8519f4be3d4e6df
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-04-27 02:02:28 +0300

    add: description

--- WeatherBot/MessagesConveyor/IOFilter/QueryPreprocessor.cs
+++ WeatherBot/MessagesConveyor/IOFilter/QueryPreprocessor.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WeatherBot.MessagesConveyor.TeleInteraction.Adapters;
 
-namespace WeatherBot.MessagesConveyor.IOFilter
-{
-    class QueryPreprocessor
-    {
+
+namespace WeatherBot.MessagesConveyor.IOFilter {
+
+    // TeleInteractor предварительно создаст у себя экземпляр этого класса, чтобы вызвать необходимый метод
+
+    class QueryPreprocessor {
+
+        // TeleInteractor вызовет этот метод при поступлении нового сообщения от пользователя, передав Message        
+
+        void MessageProcessing(Message message) { 
+
+
+            // ... здесь сообщения от пользователя, преобразуется к виду запроса
+            // т.е. создаётся QueryData и заполняется
+
+            // ну, не конкретно здесь, но отсюда стартуем чего угодно в общем ;)
+
+            // после преобразования Message.Text в QueryData отправляем базе данных
+            // пока не реализовано, раскомментится, как будет готово
+
+            // SendToDatabaseQuery(QueryData query);
+        }
     }
 }
--- WeatherBot/MessagesConveyor/IOFilter/ResponsePostprocessor.cs
+++ WeatherBot/MessagesConveyor/IOFilter/ResponsePostprocessor.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace WeatherBot.MessagesConveyor.IOFilter
-{
-    class ResponsePostprocessor
-    {
+
+namespace WeatherBot.MessagesConveyor.IOFilter {
+
+    // База данных предварительно создаст у себя экземпляр этого класса, чтобы вызвать необходимый метод
+
+    class ResponsePostprocessor {
+
+        // База данных вызовет этот метод по завершению обработки запроса, передав заполненную результатом QueryData
+
+        void QueryDataProcessing(/*QueryData query*/) {
+
+
+            // после преобразования QueryData в Message.Response.Text отправляем его обратно TeleInteractor-у
+            // пока не реализовано, раскомментится, как будет готово
+
+            // SendToUserResponse(Message message);
+        }
     }
 }
